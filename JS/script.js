cf1c708832aa692aaa2fb2fbb203582be4e619e9
Zawafe <olarewanju123@gmail.com>
2024-09-07 01:00:44 +0100

    pushing

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+/* typing words */
+var typed = new Typed(".typing",{
+    strings:["","Web Designer", "Web Developer","Front-End Developer", "Back-End Developer"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop:true
+})
